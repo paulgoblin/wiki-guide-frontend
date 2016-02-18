b32d7572b9cfbc4a57be378c3784b71d85c57296
paulgoblin <mpr75@cornell.edu>
2016-02-18 13:57:55 -0800

    pushing to gh-pages

---
+++ dist/js/shared/loginModal/loginModal.js
@@ -0,0 +1,48 @@
+'use strict';
+angular.module('wikiApp')
+
+.directive('loginModal', function(){
+  return {
+    restrict: 'E',
+    replace: true,
+    controller: 'loginCtrl',
+    controllerAs: 'lm',
+    scope: true,
+    bindToController: {
+    },
+    templateUrl:'js/shared/loginModal/loginModal.html',
+  }
+})
+
+.controller('loginCtrl', function($scope, $state, LoginSrvc) {
+
+  var lm = this;
+  lm.token = LoginSrvc.token;
+  lm.submitLogin = (loginInfo) => {
+    LoginSrvc.login(loginInfo)
+      .success( resp => {
+        $state.go('main', {login: null});
+      })
+      .error( err => {
+        lm.loginAlert = err;
+        console.log("error", err);
+      })
+  }
+  lm.submitRegister = (registerInfo) => {
+    LoginSrvc.register(registerInfo)
+      .success( resp => {
+        $state.go('main', {login: null});
+      })
+      .error( err => {
+        lm.registerAlert = err;
+        console.log("error", err);
+      })
+  }
+  lm.closeLoginModal = () => {
+    window.location.hash = window.location.hash.replace(/\?.*/,'')
+  }
+  lm.closeLoginAlert = () =>  lm.loginAlert = null;
+  lm.closeRegisterAlert = () =>  lm.registerAlert = null;
+
+
+});
