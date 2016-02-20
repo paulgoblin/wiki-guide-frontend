7f730b6471c6c10e259c3e2d5c7d7e0ebbb26836
paulgoblin <mpr75@cornell.edu>
2016-02-19 18:34:50 -0800

    push to gh-pages

---
+++ dist/js/app.module.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('wikiApp', [
+  'ui.bootstrap',
+  'ui.router',
+  'LocalStorageModule',
+])
+
+.constant('CONST', {
+  // API_URL: 'http://desolate-sea-75202.herokuapp.com',
+  API_URL: 'http://localhost:3000',
+  INITIAL_SEARCH_RAD: '10',  // miles
+  MAX_SEARCH_RAD: '1281',  // miles
+  REFRESH_DIST: '1', //how far your positon must change before deck updates, in miles
+})
+
+.constant('HELPERS', {
+  calcDist: (coords1, coords2) => {
+    let sigFigs = 2;
+    let milesPerDegreeLat = 69.2;
+    let delx = (coords1.long - coords2.long)*((180 - Math.abs(coords1.lat))/180);
+    let dely = coords1.lat - coords2.lat;
+    let delx2 = Math.pow(delx,2);
+    let dely2 = Math.pow(dely,2);
+    let dist = Math.sqrt(delx2 + dely2)*milesPerDegreeLat;
+    return dist.toPrecision(sigFigs);
+  }
+})
+
+.run(function (UserSrvc, LoginSrvc) {
+  UserSrvc.locate();
+  LoginSrvc.init(UserSrvc.requestMe);
+})
---
+++ dist/js/shared/listPage/listPage.js
@@ -0,0 +1,49 @@
+'use strict';
+angular.module('wikiApp')
+
+.directive('listPage', function(){
+  return {
+    restrict: 'E',
+    replace: true,
+    controller: 'listPageCtrl',
+    controllerAs: 'lp',
+    scope: true,
+    bindToController: {
+    },
+    templateUrl:'js/shared/listPage/listPage.html',
+  }
+})
+.controller('listPageCtrl', function($scope, $state, UserSrvc, ResourceSrvc, HELPERS) {
+  let lp = this;
+  lp.me = UserSrvc.me || { likes: [] };
+  lp.coords = UserSrvc.coords.lat ? UserSrvc.coords : null;
+  lp.nearby = UserSrvc.nearby;
+  lp.ratingScale = 10;
+  lp.legend = [...Array(lp.ratingScale)].map((_,i) => `rating${i+1}`).reverse();
+
+  lp.viewResource = (resource) => {
+    ResourceSrvc.setWell(resource);
+    $state.go('resource', {resourceId: resource._id});
+  }
+  lp.sortOrder = (resource) => {
+    if (!lp.nearby) return 'index';
+    return -UserSrvc.likesDistDict[resource.pageid];
+  }
+  lp.ratingClass = (resource) => {
+    if (!lp.nearby) return;
+    let rating = Math.ceil(lp.ratingScale*UserSrvc.likesDistDict[resource.pageid]);
+    return `rating${rating}`;
+  }
+  lp.togNearby = () => {
+    UserSrvc.nearby = !UserSrvc.nearby;
+  }
+
+  // listners
+  UserSrvc.listen('me', $scope, () => {
+    lp.me = UserSrvc.me;
+  })
+  UserSrvc.listen('coords', $scope, () => {
+    lp.coords = UserSrvc.coords;
+  })
+
+});
